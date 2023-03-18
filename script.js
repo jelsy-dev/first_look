ca0ca8c2334198a8de004cb898611fd1cc38eafe
jeljoyana <128148547+jelsy-dev@users.noreply.github.com>
2023-03-18 13:13:43 +0530

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+function validate()
+{
+  
+   var a = document.getElementById("demo").value;
+   alert("Hi "+a+"! Welcome to YC, . Wish you a great IT career!!")
+document.getElementById("res").innerHTML="Hi "+a+"! Welcome to YC, "+a+". Wish you a great IT career!!"
+return false;
+}
